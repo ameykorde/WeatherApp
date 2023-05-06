f59e898f4193882616a7de8a82d0f8bc5d389811
ameykorde <72167288+ameykorde@users.noreply.github.com>
2023-05-06 12:47:44 +0530

    added mobile link

---
+++ redirect.js
@@ -0,0 +1,4 @@
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    // User is on a mobile device, redirect to mobile site
+    window.location.href = "https://ameyweatherapp.netlify.app/";
+}
